b95943155762bdf3962242c96218ed6c598bbcd3
Bryan Woodard <bwoodard40@gmail.com>
2018-07-11 15:08:08 -0700

    Initial Commit
    Provides functionality to grab user emails in ZD

---
+++ zd_email_bookmarklet.js
@@ -0,0 +1,38 @@
+/*
+Simple Script to grab emails from ZD
+
+Add Following code as a bookmarklet
+
+Instructions to use:
+	- wait until page loads
+	- expand box to show all cc's
+	- hover over each one until you see the tooltip appear
+	- run bookmarklet
+*/
+
+javascript:(function(){
+	var email_array=[];
+	var ccs = document.querySelectorAll(".current_collaborators ul li:not(.zd-tag-shortener)");
+	for(var i = 0; i<ccs.length; i++){
+		/*debugger;*/
+		email_array.push(ccs[i].getAttribute("data-original-title"));
+
+	};
+	
+	console.log(email_array);
+	if(email_array.length == 0 && email_array[0] == null){
+		alert("Make sure you expand the cc's box and hover over each person until the tooltip shows with their email.  Try it again");
+	}else{
+		/*/alert(email_array);*/
+		debugger;
+		email_array = email_array.toString();
+		prompt("You can now copy these with CMD + C and paste them into your meeting invite", email_array);
+	}
+	
+})();
+
+
+
+
+
+
